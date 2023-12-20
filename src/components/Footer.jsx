7c4bb6c3762166b21a7409249173c5fb182f7235
Fayiz <fayizcj94.com>
2023-12-20 18:09:17 +0530

    Footer Setting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,21 @@
 
+// Footer.jsx
+// import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer style={{ backgroundColor: 'black', color: 'white', padding: '20px 0' }}>
+        <div style={{ maxWidth: '960px', margin: '0 auto', textAlign: 'center' }}>
+          <p>NTWIST(logo)</p>
+          <nav>
+            <a href="/">Home</a> | <a href="/about">About Us</a> | <a href="/contact">Contact Us</a> |{' '}
+            <a href="/privacy">Privacy Policy</a> | <a href="/sitemap">Sitemap</a>
+          </nav>
+          <p>9650 20 Ave NW, Edmonton, AB, T6N 1G1, Canada</p>
+          <p>&copy; 2022. Ntwist Inc.</p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,58 @@
-// Header.jsx
-
+import { useState, useEffect, useRef } from 'react';
+// import './Header.css'; 
 
 const Header = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = (event) => {
+    // Check if the click is outside the dropdown
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setDropdownOpen(false);
+    }
+  };
+
+  const preventClose = (event) => {
+    // Prevent closing when clicking inside the dropdown content
+    event.stopPropagation();
+  };
+
+  // Add event listeners to close dropdown when clicking outside of it
+  useEffect(() => {
+    document.addEventListener('click', closeDropdown);
+    return () => {
+      document.removeEventListener('click', closeDropdown);
+    };
+  }, []);
+
   return (
-    <header style={{ backgroundColor: 'white', color: 'black', padding: '20px 0', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <header className="header-container">
       <div>
         {/* Ntwist logo or text */}
-        <img src="/path-to-your-logo.png" alt="Ntwist logo" style={{ width: '50px', height: 'auto' }} />
+        <img src="/path-to-your-logo.png" alt="Ntwist logo" className="logo" />
       </div>
 
       <nav>
-        <ul style={{ listStyle: 'none', display: 'flex', gap: '20px', margin: 0 }}>
-          <li><a href="/">Home</a></li>
-          <li>
-            <a href="/industries">Industries</a>
-            <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
-              <li><a href="/sustainability">Sustainability</a></li>
-              <li><a href="/mineral-processing">Mineral Processing</a></li>
-              <li><a href="/mine-optimization">Mine-To-Mill-To-Mine Optimization</a></li>
-              <li><a href="/oil-gas">Oil & Gas</a></li>
-            </ul>
+        <ul className="nav-list">
+          <li><a href="/" className="bold-black">Home</a></li>
+          <li className="dropdown" onClick={handleDropdownToggle} ref={dropdownRef}>
+            <a href="/industries" className="bold-black">Industries</a>
+            {isDropdownOpen && (
+              <div className="dropdown-content" onClick={preventClose}>
+                <a href="/sustainability" className="bold-black">Sustainability</a>
+                <a href="/mineral-processing" className="bold-black">Mineral Processing</a>
+                <a href="/mine-optimization" className="bold-black">Mine-To-Mill-To-Mine Optimization</a>
+                <a href="/oil-gas" className="bold-black">Oil & Gas</a>
+              </div>
+            )}
           </li>
-          <li><a href="/ai-software">AI Software</a></li>
-          <li><a href="/blog">Blog</a></li>
-          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/ai-software" className="bold-black">AI Software</a></li>
+          <li><a href="/blog" className="bold-black">Blog</a></li>
+          <li><a href="/contact" className="bold-black">Contact Us</a></li>
         </ul>
       </nav>
     </header>
